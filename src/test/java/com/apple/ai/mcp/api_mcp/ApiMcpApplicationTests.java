e6137dfc1f25626fb89555edc8883c943398abc4
Sandip Chitale <s.chitale@apple.com>
2025-07-10 14:19:56 -0700

    INitial commit.

---
+++ src/test/java/com/apple/ai/mcp/api_mcp/ApiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.ai.mcp.api_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
